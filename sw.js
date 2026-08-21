76b458a722abe0ebb492aa290225e28c3be0c299
joo1alaricc <erlicjonathanalaric@gmail.com>
2026-08-22 00:11:42 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('notificationclick',event=>{event.notification.close();event.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(list=>{for(const c of list){if('focus' in c)return c.focus();}return clients.openWindow('/');}));});
